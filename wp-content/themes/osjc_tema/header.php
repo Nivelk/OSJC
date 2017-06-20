5f9870967d20caf2c358e9de80da55f6a600a9ad
42143 <aslan-k@hotmail.com>
2017-06-20 06:50:05 -0300

    New themes/ OSJC tema

---
+++ wp-content/themes/osjc_tema/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> >
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="<?php  echo $header_setting['upload_image_favicon']; ?>" /> 
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+        <?php wp_head(); ?>
+    </head>
+    <body <?php body_class(); ?>>
+    
+    </body>
