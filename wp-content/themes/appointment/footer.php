3447507f5ab694974ae380906ec4c14919a18fd6
42143 <aslan-k@hotmail.com>
2017-06-27 03:41:04 -0300

    atualização themes

--- wp-content/themes/appointment/footer.php
+++ wp-content/themes/appointment/footer.php
@@ -6,23 +6,19 @@ if ( is_active_sidebar( 'footer-widget-area' ) ) { ?>
 <div class="footer-section">
 	<div class="container">	
 		<div class="row footer-widget-section">
-			<?php  dynamic_sidebar( 'footer-widget-area' );	} ?>	
-			<div class="clearfix"></div>
-            <div class="col-md-4">
-                <div id="fb-root"></div>
+			<?php  dynamic_sidebar( 'footer-widget-area' );	} ?>
+            <div class="col-md-3">
+                    <div id="fb-root"></div>
                     <script>(function(d, s, id) {
                       var js, fjs = d.getElementsByTagName(s)[0];
                       if (d.getElementById(id)) return;
                       js = d.createElement(s); js.id = id;
-                      js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.9&appId=674383036103265";
+                      js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.9";
                       fjs.parentNode.insertBefore(js, fjs);
                     }(document, 'script', 'facebook-jssdk'));</script>
-
-
-
-                 <div class="fb-page" data-href="https://www.facebook.com/obrassociaisdojardimclimax/?hc_ref=SEARCH&amp;fref=nf" data-tabs="timeline" data-width="350" data-height="150" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/obrassociaisdojardimclimax/?hc_ref=SEARCH&amp;fref=nf" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/obrassociaisdojardimclimax/?hc_ref=SEARCH&amp;fref=nf">Obras Sociais do Jardim Clímax</a></blockquote>
-                </div>
+                        <div class="fb-page" data-href="https://www.facebook.com/obrassociaisdojardimclimax/" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/obrassociaisdojardimclimax/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/obrassociaisdojardimclimax/">Obras Sociais do Jardim Clímax</a></blockquote></div>
             </div>
+			<div class="clearfix"></div>
 		</div>
 	</div>
 </div>
